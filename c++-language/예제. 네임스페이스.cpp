09a5a77a9c19e70fb81dc65b98e8ec30e028b915
박민경 <ness91011@gmail.com>
2026-06-28 16:51:50 +0900

    C++ namespace 학습

---
+++ c++-language/예제. 네임스페이스.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+
+namespace BestComImpl {
+    void SimpleFunc(void);
+}
+
+namespace BestComImpl {
+    void PrettyFunc(void);
+}
+
+namespace ProgComImpl {
+    void SimpleFunc(void);
+}
+
+int main() {
+    BestComImpl::SimpleFunc();
+    return 0;
+}
+
+void BestComImpl::SimpleFunc(void) {
+    std::cout<<"BestCom이 정의한 함수"<<std::endl;
+    PrettyFunc();
+    ProgComImpl::SimpleFunc();
+}
+
+void BestComImpl::PrettyFunc(void) {
+    std::cout<<"So Pretty!!"<<std::endl;
+}
+
+void ProgComImpl::SimpleFunc(void) {
+    std::cout<<"ProgCom이 정의한 함수"<<std::endl;
+}
