a0b2d126a60d853c5c02a8530d47569472e6c32e
박민경 <ness91011@gmail.com>
2026-06-29 15:19:17 +0900

    오늘 C언어 복습함.

---
+++ c++-language/01-4/Souce1.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include "01-4.h"
+
+void BestComImpl::SimpleFunc(void) {
+    std::cout<<"BestCom이 정의한 함수"<<std::endl;
+}
+
+void ProgComImpl::SimpleFunc(void) {
+    std::cout<<"ProgCom이 정의한 함수"<<std::endl;
+}
---
+++ c++-language/01-4/main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include "01-4.h"
+
+int main(void) {
+    BestComImpl::SimpleFunc();
+    ProgComImpl::SimpleFunc();
+    return 0;
+}
